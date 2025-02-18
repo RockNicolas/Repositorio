99b9cc3f3f909494c6192999d7f202918dbe90f4
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-02-18 13:23:54 -0300

    Criação de efeitos de icons

--- src/pages/Header/MainContent.tsx
+++ src/pages/Header/MainContent.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import './mainContent.css'; 
+import { FaReact, FaHtml5, FaCss3Alt, FaJsSquare, FaPython, FaPhp, FaNodeJs, FaDatabase } from 'react-icons/fa';  // Importando o ícone de banco de dados (MySQL)
+import { SiTypescript, SiLaravel, SiVuedotjs } from 'react-icons/si';  
+import './mainContent.css';
 
 const MainContent: React.FC = () => {
   return (
@@ -9,8 +11,22 @@ const MainContent: React.FC = () => {
         <h2 className="sub-title">
           Sou um Desenvolvedor Web
           <br />
-          Especializado em React, TypeScript, HTML, CSS e JavaScript
+          Especializado em React, TypeScript, HTML, CSS, JavaScript, Python, PHP, Node.js, Laravel, Vue.js, MySQL
         </h2>
+
+        <div className="icons-container">
+          <FaReact className="icon react" />
+          <FaHtml5 className="icon html" />
+          <FaCss3Alt className="icon css" />
+          <FaJsSquare className="icon js" />
+          <SiTypescript className="icon ts" />
+          <FaPython className="icon python" />
+          <FaPhp className="icon php" />
+          <FaNodeJs className="icon node" />
+          <SiLaravel className="icon laravel" />
+          <SiVuedotjs className="icon vue" />  
+          <FaDatabase className="icon mysql" />
+        </div>
       </main>
     </div>
   );
