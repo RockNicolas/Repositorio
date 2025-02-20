711e94fac6f037fa0a34fd5269c247e0684e3104
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-02-20 15:44:33 -0300

    Criação de .env

--- src/Hook/useGameStatus.ts
+++ src/Hook/useGameStatus.ts
@@ -5,14 +5,20 @@ const useGameStatus = () => {
 
   const fetchGameStatus = async () => {
     try {
-      const response = await fetch('http://127.0.0.1:5000/run-snake-game');
+      const apiUrl = process.env.REACT_APP_API_URL;
+      if (!apiUrl) {
+        setGameStatus('URL da API não configurada.');
+        return;
+      }
+
+      const response = await fetch(apiUrl);
       const data = await response.json();
 
       if (data.error) {
         setGameStatus(`Erro: ${data.error}`);
       } else {
         setGameStatus(data.message);
-        console.log(data.output);               
+        console.log(data.output);
       }
     } catch (error) {
       console.error("Erro ao executar o script Python:", error);
